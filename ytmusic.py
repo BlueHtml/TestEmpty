6d328bf0dc7876c61de30ea107f51c4bd4bded19
BlueHtml <luxiaofeng006@qq.com>
2021-02-28 00:52:30 +0800

    Update ytmusic.py

--- ytmusic.py
+++ ytmusic.py
@@ -96,7 +96,7 @@ def main(args):
 
     init()
 
-    search_query = "https://music.youtube.com/watch?v=ks7CfcUZHII"
+    search_query = "Day Not You"
 
     try:
         result = ydl.extract_info(
