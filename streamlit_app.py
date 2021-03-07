2001cd48591befefa1c928e031e2dfa6c08a9f85
BlueHtml <luxiaofeng006@qq.com>
2021-03-07 19:10:46 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
+# -*- coding: utf-8 -*-
+
+import streamlit as st
+
 # Streamlit encourages well-structured code, like starting execution in a main() function.
 def main():
     print('hello world')
+    st.markdown('# 一级标题')
+    st.markdown('**粗体**')
+    st.markdown('*斜体*')
+    st.markdown('~~删除~~')
 
 if __name__ == "__main__":
     main()
